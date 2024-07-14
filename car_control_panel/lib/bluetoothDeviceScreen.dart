57f307ff61a80675404cfe1f4587df8cbe5b55ed
Sarp Arslan <sarparslan03@gmail.com>
2024-07-14 22:45:15 +0300

    Update

--- car_control_panel/lib/bluetoothDeviceScreen.dart
+++ car_control_panel/lib/bluetoothDeviceScreen.dart
@@ -3,119 +3,75 @@ import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
 
 class BluetoothDeviceScreen extends StatefulWidget {
-  const BluetoothDeviceScreen({super.key});
-
   @override
-  State<BluetoothDeviceScreen> createState() => _BluetoothDeviceScreenState();
+  _BluetoothDeviceScreenState createState() => _BluetoothDeviceScreenState();
 }
 
 class _BluetoothDeviceScreenState extends State<BluetoothDeviceScreen> {
   FlutterBlue flutterBlue = FlutterBlue.instance;
-  List<ScanResult> scanResults = [];
-  bool isScanning = false;
+  List<BluetoothDevice> devicesList = [];
 
   @override
   void initState() {
     super.initState();
-    _startScan();
-  }
-
-  void _startScan() {
-    setState(() {
-      isScanning = true;
-      scanResults.clear();
-    });
-
-    flutterBlue.startScan(timeout: Duration(seconds: 4)).then((_) {
-      setState(() {
-        isScanning = false;
-      });
-    });
+    flutterBlue.startScan(timeout: Duration(seconds: 4));
 
     flutterBlue.scanResults.listen((results) {
-      setState(() {
-        scanResults = results;
-        print("Scan results updated: ${results.length} devices found");
-      });
+      for (ScanResult result in results) {
+        if (!devicesList.contains(result.device)) {
+          setState(() {
+            devicesList.add(result.device);
+          });
+        }
+      }
     });
+
+    flutterBlue.stopScan();
   }
 
-  void _connectToDevice(BluetoothDevice device) async {
+  void connectToDevice(BluetoothDevice device) async {
     try {
       await device.connect();
       Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) => ControlScreen(device: device),
-        ),
-      );
+          context,
+          MaterialPageRoute(
+              builder: (context) => ControlScreen(device: device)));
     } catch (e) {
-      // Hata durumunda kullanıcıya bilgi ver
       showDialog(
         context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: Text("Connection Error"),
-            content: Text("Failed to connect to the device: $e"),
-            actions: <Widget>[
-              TextButton(
-                child: Text("OK"),
-                onPressed: () {
-                  Navigator.of(context).pop(); // Dialog'u kapat
-                },
-              ),
-            ],
-          );
-        },
+        builder: (context) => AlertDialog(
+          title: Text("Error"),
+          content: Text("Failed to connect."),
+          actions: <Widget>[
+            ElevatedButton(
+              child: Text("Close"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            )
+          ],
+        ),
       );
-      print("Connection failed: $e");
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Select Bluetooth Device'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.refresh),
-            onPressed: _startScan,
-          ),
-        ],
+      appBar: AppBar(title: Text('Available Bluetooth Devices')),
+      body: ListView.builder(
+        itemCount: devicesList.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(devicesList[index].name),
+            subtitle: Text(devicesList[index].id.toString()),
+            trailing: ElevatedButton(
+              child: Text('Connect'),
+              onPressed: () => connectToDevice(devicesList[index]),
+            ),
+          );
+        },
       ),
-      body: isScanning
-          ? Center(child: CircularProgressIndicator())
-          : scanResults.isEmpty
-              ? Center(child: Text('No devices found.'))
-              : ListView.builder(
-                  itemCount: scanResults.length,
-                  itemBuilder: (context, index) {
-                    ScanResult result = scanResults[index];
-                    return Card(
-                      color: Colors.blue.shade50,
-                      margin:
-                          EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-                      child: ListTile(
-                        leading: Icon(Icons.bluetooth, color: Colors.blue),
-                        title: Text(
-                          result.device.name.isNotEmpty
-                              ? result.device.name
-                              : result.device.id.toString(),
-                          style: TextStyle(color: Colors.blue.shade800),
-                        ),
-                        trailing: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            primary: Colors.blue, // Button color
-                            onPrimary: Colors.white, // Text color
-                          ),
-                          onPressed: () => _connectToDevice(result.device),
-                          child: Text('Connect'),
-                        ),
-                      ),
-                    );
-                  },
-                ),
     );
   }
 }
--- car_control_panel/lib/controlScreen.dart
+++ car_control_panel/lib/controlScreen.dart
@@ -1,146 +1,83 @@
-import 'dart:convert';
-import 'dart:math';
-import 'dart:typed_data';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
-import 'package:flutter_joystick/flutter_joystick.dart';
 
 class ControlScreen extends StatefulWidget {
   final BluetoothDevice device;
 
-  const ControlScreen({required this.device, super.key});
+  ControlScreen({required this.device});
 
   @override
-  State<ControlScreen> createState() => _ControlScreenState();
+  _ControlScreenState createState() => _ControlScreenState();
 }
 
 class _ControlScreenState extends State<ControlScreen> {
-  FlutterBlue flutterBlue = FlutterBlue.instance;
-  BluetoothDevice? _connectedDevice;
-  BluetoothCharacteristic? _characteristic;
-  bool _isConnected = false;
+  BluetoothCharacteristic? writableCharacteristic;
 
   @override
   void initState() {
     super.initState();
-    _connectToDevice();
+    discoverServices();
   }
 
-  void _connectToDevice() async {
-    // Attempt to connect to the device
-    await widget.device.connect().catchError((e) {
-      print('Failed to connect: $e');
-    });
-
-    // Listen to connection state updates
-    widget.device.state.listen((state) {
-      setState(() {
-        _isConnected = (state == BluetoothDeviceState.connected);
-      });
-    });
-
-    // Discover services after connection
-    if (_isConnected) {
-      _discoverServices();
-    }
-  }
-
-  void _discoverServices() async {
+  void discoverServices() async {
     List<BluetoothService> services = await widget.device.discoverServices();
     for (var service in services) {
       for (var characteristic in service.characteristics) {
         if (characteristic.properties.write) {
           setState(() {
-            _characteristic = characteristic;
+            writableCharacteristic = characteristic;
           });
+          return; // Stop looking once a writable characteristic is found
         }
       }
     }
+
+    if (writableCharacteristic == null) {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text("Error"),
+          content: Text("No writable characteristic found."),
+          actions: <Widget>[
+            ElevatedButton(
+              child: Text('OK'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+          ],
+        ),
+      );
+    }
   }
 
-  void _sendCommand(int command) {
-    if (_characteristic != null && _isConnected) {
-      var commandBytes = Uint8List(1);
-      commandBytes[0] = command;
-      _characteristic!.write(commandBytes).then((_) {
-        print("Command sent successfully");
-      }).catchError((error) {
-        print("Failed to send command: $error");
-      });
-    } else {
-      print("Characteristic not found or device is not connected.");
+  void sendData(String data) async {
+    if (writableCharacteristic == null) {
+      print("No writable characteristic set.");
+      return;
+    }
+
+    try {
+      await writableCharacteristic!.write(data.codeUnits);
+      print("Data sent successfully.");
+    } catch (e) {
+      print("Error sending data: $e");
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Text('Bluetooth Car Control'),
-      ),
+      appBar: AppBar(title: Text('Control Device')),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextButton(
-                onPressed: () {
-                  _sendCommand(0);
-                },
-                child: Text("Pick me ")),
-            SizedBox(
-              height: 20,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () => sendData("0"),
+              child: Text('Send Data'),
             ),
-            Joystick(
-              base: JoystickBase(
-                decoration: JoystickBaseDecoration(
-                  middleCircleColor: Colors.red.shade400,
-                  drawOuterCircle: false,
-                  drawInnerCircle: false,
-                  boxShadowColor: Colors.red.shade100,
-                ),
-              ),
-              stick: JoystickStick(
-                decoration: JoystickStickDecoration(
-                  color: Colors.red,
-                ),
-              ),
-              listener: (details) {
-                int command = _calculateCommand(details);
-                _sendCommand(command);
-              },
-            ),
-            SizedBox(height: 20),
-            Text(_connectedDevice != null
-                ? 'Connected to ${_connectedDevice!.name}'
-                : 'Scanning for devices...'),
-            SizedBox(height: 20),
           ],
         ),
       ),
     );
-  }
-
-  int _calculateCommand(StickDragDetails details) {
-    double angle = (atan2(details.y, details.x) * 180 / pi + 360) % 360;
-
-    if (angle < 22.5 || angle >= 337.5) {
-      return 0; // Saat 12 yönü
-    } else if (angle >= 22.5 && angle < 67.5) {
-      return 2; // Saat 12 - 15 yönü arası
-    } else if (angle >= 67.5 && angle < 112.5) {
-      return 7; // Saat 15 yönü
-    } else if (angle >= 112.5 && angle < 157.5) {
-      return 5; // Saat 15 - 18 yönü arası
-    } else if (angle >= 157.5 && angle < 202.5) {
-      return 3; // Saat 18 yönü
-    } else if (angle >= 202.5 && angle < 247.5) {
-      return 4; // Saat 18 - 21 yönü arası
-    } else if (angle >= 247.5 && angle < 292.5) {
-      return 6; // Saat 21 yönü
-    } else {
-      return 0; // Geri kalan durumlar için varsayılan
-    }
   }
 }
